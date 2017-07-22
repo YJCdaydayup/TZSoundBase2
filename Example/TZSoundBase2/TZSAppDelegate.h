05b79d4adf5f675fb2ee9e9465890d83affecfb3
杨力 <jeff.qq.com>
2017-07-22 15:59:02 +0800

    Initial commit

---
+++ Example/TZSoundBase2/TZSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TZSAppDelegate.h
+//  TZSoundBase2
+//
+//  Created by yangli123456 on 07/22/2017.
+//  Copyright (c) 2017 yangli123456. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TZSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/TZSoundBase2/TZSViewController.m
@@ -0,0 +1,29 @@
+//
+//  TZSViewController.m
+//  TZSoundBase2
+//
+//  Created by yangli123456 on 07/22/2017.
+//  Copyright (c) 2017 yangli123456. All rights reserved.
+//
+
+#import "TZSViewController.h"
+
+@interface TZSViewController ()
+
+@end
+
+@implementation TZSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
